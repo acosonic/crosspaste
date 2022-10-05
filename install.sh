06927044d13f77ccee8d55d05900bad54290244f
Aleksandar Pavić <acosonic@gmail.com>
2022-10-05 08:33:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 apt install -y xdotool xclip
-wget https://github.com/acosonic/crosspaste/raw/main/crosspaste.png -P $HOME
-cat <<EOF >/usr/share/applications/crosspaste.desktop
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+wget https://github.com/acosonic/crosspaste/raw/main/crosspaste.png -P $USER_HOME
+sudo cat <<EOF >/usr/share/applications/crosspaste.desktop
 [Desktop Entry]
 Name=Cross Paste
-Exec=/bin/bash -c "sh -c 'sleep 1.5; xdotool type \"\$(xclip -o -selection clipboard)\"'"
+Exec=/bin/bash -c "sh -c 'sleep 2.5; xdotool type \"\$(xclip -o -selection clipboard)\"'"
 Type=Application
 Terminal=false
-Icon=${HOME}/crosspaste.png
+Icon=${USER_HOME}/crosspaste.png
 EOF
