cd91f5caa83ce54ed56c3a5f1a0c0493b272279b
Aleksandar Pavić <acosonic@gmail.com>
2022-10-05 07:42:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 apt install -y xdotool xclip
-wget ... -P /usr/share/applications/
+me="$(whoami)"
+wget ... -P /usr/share/applications/crosspaste.png
+cat <<EOF >/usr/share/applications/crosspaste.desktop
+[Desktop Entry]
+Name=Cross Paste
+Exec=/bin/bash -c "sh -c 'sleep 1.5; xdotool type \"$(xclip -o -selection clipboard)\"'"
+Type=Application
+Terminal=false
+Icon=/home/$*{me}/crosspaste.png
+EOF
